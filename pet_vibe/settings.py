842d3133b2ada351c7e893abcd32598b3fba0a65
Thiago Sobral Alves <thiagosobral23@gmail.com>
2024-11-23 12:53:38 +0000

    Removed secret key and set debug

--- pet_vibe/settings.py
+++ pet_vibe/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#3ya96um1#m-@zp=3#&&#1&k1cco-#b%l7tqx@w=4vw+5f#0cn'
+os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 # ALLOWED_HOSTS = ['8000-thiago23-petvibe-f55we9hgp7f.ws.codeinstitute-ide.net']
 ALLOWED_HOSTS = [
