a9a845756d1719526eb93c3a0f62326ec129cfa6
Thiago Sobral Alves <thiagosobral23@gmail.com>
2024-12-01 16:36:58 +0000

    Style applied to form to match the project gold color and update the conatct form

--- contact/forms.py
+++ contact/forms.py
@@ -7,4 +7,10 @@ class ContactForm(forms.ModelForm):
     """
     class Meta:
         model = Contact
-        fields = '__all__'
+        fields = ('name', 'email', 'enquiry_type', 'message',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-gold'
