47736e693efca734575ebe53165161c7f757b83e
Thiago Sobral Alves <thiagosobral23@gmail.com>
2024-12-02 15:59:55 +0000

    added enquiried views, ulrs and template

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ContactUs.as_view(), name='contact'),
+    path('enquiries/', views.EnquiriesListView.as_view(), name='enquiries'),
+    path('enquiries/<int:pk>/', views.EnquiryDetailView.as_view(), name='enquiry_detail'),
+    path('enquiries/delete/<int:pk>/', views.DeleteEnquiryView.as_view(), name='delete_enquiry'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.views import generic, View
+from django.views.generic import DeleteView
 from django.conf import settings
 from django.template.loader import render_to_string
 from django.urls import reverse_lazy
@@ -51,3 +52,39 @@ class ContactUs(View):
         else:
             messages.error(request, "There was an error submitting the form. Please try again.")
         return render(request, "contact/contact.html", {"contact_form": form})
+
+class EnquiriesListView(generic.ListView):
+    """
+    Displays a list of all enquiries for admin.
+    """
+    model = Contact
+    template_name = "contact/enquiries_dashboard.html"
+    context_object_name = "enquiries"
+
+    def get_queryset(self):
+        return Contact.objects.all().order_by("-date")
+
+
+class EnquiryDetailView(generic.DetailView):
+    """
+    Displays details of a specific enquiry.
+    """
+    model = Contact
+    template_name = "contact/enquiry_detail.html"
+    context_object_name = "enquiry"
+
+
+class DeleteEnquiryView(DeleteView):
+    """
+    This view allows admin to delete an enquiry.
+    """
+    model = Contact
+    template_name = "contact/delete_enquiry.html"
+    success_url = reverse_lazy('enquiries')
+
+    def delete(self, request, *args, **kwargs):
+        """
+        Override delete to add a success message.
+        """
+        messages.success(request, "Enquiry deleted successfully!")
+        return super().delete(request, *args, **kwargs)
